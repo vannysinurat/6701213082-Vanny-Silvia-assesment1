8756fe08f06a0f36362dfe99e398e918192fd3fe
PAWB <vanny.com>
2023-03-30 09:06:13 +0700

    mengisi pemrosesan dengan inputan form.html ke proses.php

---
+++ OneDrive/Dokumen/PABWBARU/assesment1/proses.php
@@ -0,0 +1,19 @@
+<?php
+$angka1 = $_POST["angka1"];
+$angka2 = $_POST["angka2"];
+$operator = $_POST["operator"];
+
+if ($operator == "+") {
+	$hasil = $angka1 + $angka2;
+} else if ($operator == "-") {
+	$hasil = $angka1 - $angka2;
+} else if ($operator == "*") {
+	$hasil = $angka1 * $angka2;
+} else if ($operator == "/") {
+	$hasil = $angka1 / $angka2;
+} else {
+	$hasil = "Operator tidak valid";
+}
+
+echo "Hasil operasi: " . $hasil;
+?>
